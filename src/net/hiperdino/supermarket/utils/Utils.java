0dda3434747dbe01964dce020285359365e78c97
ravel01106 <ravello2002@gmail.com>
2023-04-28 14:01:50 +0100

    feat(Utils): add the Utils class

---
+++ src/net/hiperdino/supermarket/utils/Utils.java
@@ -0,0 +1,63 @@
+package net.hiperdino.supermarket.utils;
+
+import java.util.Random;
+
+public class Utils {
+    static final private String[] PRODUCTS = new String[] {
+            "Biscuit",
+            "Ham",
+            "Cheese",
+            "Milk",
+            "Bread",
+            "Rice",
+            "olive",
+            "tomato",
+            "onion",
+            "lettuce",
+            "tuna",
+            "anchovy",
+            "Butter",
+            "flour",
+            "egg",
+            "spicy pork sausage",
+            "sausage",
+            "Serrano ham",
+            "Coffee",
+            "Coca"
+    };
+    static final private String[] NAME_COSTUMER = new String[] {
+            "Amanda",
+            "Adrian",
+            "Saul",
+            "Miriam",
+            "Maria",
+            "David",
+            "Marco",
+            "Pablo",
+            "Alvaro",
+            "P.Vadillo",
+            "Oliver",
+            "Alba",
+            "Tory",
+            "Jesus",
+            "Keny",
+            "Danna",
+            "Tina",
+            "Ana",
+            "Sofia",
+            "Manuel"
+    };
+
+    public static String getRandomProduct() {
+        Random numRandom = new Random();
+        int numIndex = numRandom.nextInt(PRODUCTS.length);
+        return PRODUCTS[numIndex];
+    }
+
+    public static String getRandomName() {
+        Random numRandom = new Random();
+        int numIndex = numRandom.nextInt(NAME_COSTUMER.length);
+        return NAME_COSTUMER[numIndex];
+    }
+
+}
